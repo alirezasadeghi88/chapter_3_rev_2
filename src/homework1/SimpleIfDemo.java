40b5b616538386967c324d331f917f94a0469196
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-04 04:53:05 -0800

    add SimpleIfDemo

---
+++ src/homework1/SimpleIfDemo.java
@@ -0,0 +1,17 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class SimpleIfDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0)
+            System.out.println("HiFive");
+
+        if (number % 2 == 0)
+            System.out.println("HiEven");
+    }
+}
