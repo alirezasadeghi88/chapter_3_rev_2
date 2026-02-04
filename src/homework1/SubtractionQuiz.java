5b88087c4ff25530c4fdc3fd48613fcde6cc2d69
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-04 04:58:34 -0800

    add SubtractionQuiz

---
+++ src/homework1/SubtractionQuiz.java
@@ -0,0 +1,29 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class SubtractionQuiz {
+    public static void main(String[] args) {
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
+
+        if (number1 < number2) {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+
+        System.out.print
+                ("What is " + number1 + " − " + number2 + "? ");
+        Scanner scanner = new Scanner(System.in);
+        int answer = scanner.nextInt();
+
+        if (number1 - number2 == answer)
+            System.out.println("You are correct!");
+        else {
+            System.out.println("Your answer is wrong.");
+            System.out.println(number1 + " − " + number2 +
+                    " should be " + (number1 - number2));
+        }
+    }
+}
